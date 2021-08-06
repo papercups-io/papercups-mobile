149aa78fa2f26708458d2dd2eeee9e39e5af5682
reichert621 <reichertjalex@gmail.com>
2021-08-06 15:01:04 -0400

    Set up demo login page

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -57,7 +57,7 @@ function TabOneNavigator() {
       <TabOneStack.Screen
         name="TabOneScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+        options={{ headerTitle: 'Log in' }}
       />
     </TabOneStack.Navigator>
   );
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,61 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tailwind from "tailwind-rn";
 
 export default function TabOneScreen() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={tailwind("h-full bg-white")}>
+      <View style={tailwind("mt-24 items-center")}>
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={tailwind("w-24 h-24")}
+          resizeMode={"contain"}
+        />
+      </View>
+      <View style={tailwind("mt-8 items-center")}>
+        <Text style={tailwind("text-3xl font-extralight mb-6")}>Papercups</Text>
+        <TextInput
+          style={{
+            ...tailwind("w-72 h-12 px-4 rounded-xl border-gray-400 mb-4"),
+            borderWidth: StyleSheet.hairlineWidth,
+          }}
+          placeholder="Email"
+          onChangeText={setEmail}
+          autoCapitalize={"none"}
+          autoCompleteType="off"
+          value={email}
+        />
+        <TextInput
+          style={{
+            ...tailwind("w-72 h-12 px-4 rounded-xl border-gray-400 mb-4"),
+            borderWidth: StyleSheet.hairlineWidth,
+          }}
+          placeholder="Password"
+          onChangeText={setPassword}
+          autoCapitalize={"none"}
+          autoCompleteType="off"
+          secureTextEntry={true}
+          value={password}
+        />
+
+        <TouchableOpacity
+          style={tailwind("w-72 p-2 rounded-xl bg-blue-400 mt-5 items-center")}
+          onPress={console.log}
+        >
+          <Text style={tailwind("text-white text-lg")}>Log In</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
